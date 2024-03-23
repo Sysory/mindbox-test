10b0371936b3faa1b7c0f53ad044b9ac158f2160
Jacqueli <jacqueli@student.21-school.ru>
2024-03-23 20:22:50 +0300

    доп проверки треугольника, определение на прямоугольность

--- src/libarea.py
+++ src/libarea.py
@@ -5,33 +5,36 @@ class Figure:
   isValid = False
 # class Figure(ABC):
   # @abstractmethod
-  def getSquare() -> float:
-    return None
-
+  def getArea() -> float:
+    pass
+  
 class Triangle(Figure):
+  isRect = False
 
   def __init__(self, a : float, b : float, c : float):
-    try: 
-      if (a > 0 and b > 0 and c > 0):
-        self.sides = (a, b, c)
+    try:
+      sides = (a, b, c)
+      sides = sorted(sides)
+      
+      if (a > 0 and b > 0 and c > 0 and sides[2] < sum(sides[:2])):
+        self.sides = sides
         self.isValid = True
       else:
         raise Exception
     except:
-      self.sides = (a, b, c)
-      # self.a, self.b, self.c = 0, 0, 0
+      self.sides = (0, 0, 0)
+    else:
+      if (self.sides[2]**2 == self.sides[0]**2 + self.sides[1]**2):
+        self.isRect = True
 
-  def getSquare(self):
+  def getArea(self):
     if (not self.isValid): return 0
 
     a, b, c = self.sides
-    s = (a + b + c) / 2 #полу-периметр
+    s = (a + b + c) / 2
     area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
     return area
   
-class RectTriangle(Triangle):
-  pass
-
 
 class Circle(Figure):
   def __init__(self, radius : float):
@@ -44,6 +47,6 @@ class Circle(Figure):
     except:
       self.radius = 0
 
-  def getSquare(self):
+  def getArea(self):
     return pi * self.radius * self.radius
 
--- src/main.py
+++ src/main.py
@@ -2,7 +2,11 @@ from libarea import *
 
 def main():
   tri = Triangle(3, 4, 5)
-  print(tri.getSquare())
+  print(tri.getArea())
+  print(tri.isRect)
+
+  cir = Circle(5)
+  print(cir.getArea())
 
 if __name__ == "__main__":
   main()
